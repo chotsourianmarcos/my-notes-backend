7858e12002c82ccc734dab7ec48b305b2948852e
mchotsourian <mchotsourian@sgfintech.com.ar>
2023-11-02 10:28:05 -0300

    db-migration/refactor-SOLID-OOP

---
+++ Data/Migrations/20231102131915_refactor-SOLID-OOP.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class refactorSOLIDOOP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HashedAccessToken",
+                table: "t_users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HashedAccessToken",
+                table: "t_users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
